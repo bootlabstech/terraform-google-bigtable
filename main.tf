8d83eaabb555e440ccb7ee8b26da1f6c81218c3e
Naveen Manokaran <naveen.manokaran@bootlabstech.com>
2022-07-07 15:57:42 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,42 @@
+resource "google_bigtable_instance" "production-instance" {
+  name                = var.bigtable_instance_name
+  project             = var.project_id
+  deletion_protection = var.deletion_protection
+  labels              = length(keys(var.labels)) < 0 ? null : var.labels
+  lifecycle {
+    ignore_changes = [
+      labels,
+    ]
+  }
+  cluster {
+    cluster_id   = var.cluster_name
+    storage_type = var.storage_type
+    zone         = var.zone
+    num_nodes    = var.number_of_cluster
+    kms_key_name = var.kms_key_name
+  }
+}
+
+resource "google_bigtable_table" "table" {
+  count         = length(var.table_name)
+  name          = var.table_name[count.index]
+  project       = var.project_id
+  instance_name = google_bigtable_instance.production-instance.name
+
+  dynamic "column_family" {
+    for_each = var.enable_column_family ? [{}] : []
+    content {
+      family = var.family_name
+    }
+  }
+}
+
+resource "google_bigtable_app_profile" "app_profile" {
+  for_each                      = var.enable_app_profile ? toset([{}]) : []
+  project                       = var.project_id
+  instance                      = google_bigtable_instance.production-instance.name
+  app_profile_id                = var.app_profile_id
+  multi_cluster_routing_use_any = var.multi_cluster_routing_use_any
+  ignore_warnings               = var.ignore_warnings
+}
+
